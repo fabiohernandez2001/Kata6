b55fda71414ec4d7dcd4bc682e03f9d94d73667f
fabio <fabio@DESKTOP-0P0NGSG>
2021-12-12 23:53:33 +0000

    Kata 6 versión 1

---
+++ src/main/java/toys/Car.java
@@ -0,0 +1,16 @@
+
+package toys;
+
+public class Car {
+    public int serialNumber;
+    public Car(int serialNumber){
+        this.serialNumber=serialNumber;
+    }
+    public void pack(){
+        
+    }
+    public void label(){
+        
+    }
+        
+}
